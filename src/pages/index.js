ca18b5dcd1152731045ebc583897dabd06ab42c8
KenjiKamimura <kjp.kamimura@gmail.com>
2020-04-18 21:35:45 +1200

    Adding dummy data and image

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Personal Portfolio</h1>
+    <p>Welcome to my online portfolio</p>
+    <p>Showcasing different projects of mine</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
