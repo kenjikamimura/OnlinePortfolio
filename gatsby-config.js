084ffab1646e3aebc92253100dfe0e79c67c2dee
KenjiKamimura <kjp.kamimura@gmail.com>
2020-04-19 10:17:02 +1200

    Add Fonts plugin and roboto

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,21 @@ module.exports = {
         icon: `src/images/kenji-kamimura-logo-32x32.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto Mono`,
+            variants: [`400`, `700`],
+          },
+          {
+            family: `Roboto`,
+            subsets: [`latin`],
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
