a30eb69c4c93d29c1a9369b31b18ff24df00523b
KenjiKamimura <kjp.kamimura@gmail.com>
2020-04-19 10:13:27 +1200

    Add navbar with basic styles and pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,30 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+
+const styles = {
+  container: {
+    display: "grid",
+    gridTemplateColumns: "25% auto",
+    gridTemplateRows: "100%",
+    minHeight: "100vh",
+  },
+  navbar: {
+    maxWidth: `300px`,
+    background: "white",
+    display: "flex",
+    flexDirection: "column",
+    borderRightWidth: "1px",
+    borderRightStyle: "solid",
+    borderRightColor: "rgba(0, 0, 0, 0.12)",
+  },
+  main: {
+    padding: "1rem",
+  },
+}
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,19 +38,28 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+    <div style={styles.container}>
+      <nav style={styles.navbar}>
+        <div style={{}}>{data.site.siteMetadata.title}</div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/personal-projects">Personal Projects</Link>
+          </li>
+          <li>
+            <Link to="/university-projects">Universtiy Projects</Link>
+          </li>
+          <li>
+            <Link to="/drone-cinematography">Drone Cinematography</Link>
+          </li>
+        </ul>
+      </nav>
+      <div style={styles.main}>
         <main>{children}</main>
-        <footer>© {new Date().getFullYear()}</footer>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,34 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <h1>Personal Portfolio</h1>
+    <p>Welcome to my online portfolio</p>
+    <p>Showcasing different projects of mine</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Personal Portfolio</h1>
+    <p>Welcome to my online portfolio</p>
+    <p>Showcasing different projects of mine</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Personal Portfolio</h1>
+    <p>Welcome to my online portfolio</p>
+    <p>Showcasing different projects of mine</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Personal Portfolio</h1>
+    <p>Welcome to my online portfolio</p>
+    <p>Showcasing different projects of mine</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
