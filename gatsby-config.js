a083977716c90c3918bf63ee08bd27908d74dda9
KenjiKamimura <kjp.kamimura@gmail.com>
2020-04-25 16:50:49 +1200

    Add gatsby-source-filesystem and gatsby-transformer-remark plugins and confgure them

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,14 @@ module.exports = {
         ],
       },
     },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown/`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
