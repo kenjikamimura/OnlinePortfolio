4e61a0541c2e8c0db3c8d7abcf8b18138e72be40
KenjiKamimura <kjp.kamimura@gmail.com>
2020-04-26 15:16:42 +1200

    Refactor Layout component to use tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,92 +11,48 @@ import NavbarItem from "./NavbarItem"
 import logo from "../images/logo-black.png"
 import "./layout.css"
 
-const styles = {
-  container: {
-    display: "grid",
-    gridTemplateColumns: "280px auto",
-    gridTemplateRows: "100%",
-    minHeight: "100vh",
-  },
-  navbar: {
-    background: "white",
-    display: "flex",
-    flexDirection: "column",
-    borderRightWidth: "1px",
-    borderRightStyle: "solid",
-    borderRightColor: "rgba(0, 0, 0, 0.12)",
-  },
-  main: {
-    padding: "1rem",
-  },
-  logo: {
-    width: "40px",
-    marginTop: "0.5rem",
-    marginBottom: "0.5rem",
-    marginLeft: "0.5rem",
-    marginRight: "1.6rem",
-  },
-  navbarIcons: {
-    height: "1.5rem",
-    width: "1.5rem",
-    marginLeft: "0.2rem",
-    marginRight: "0.8rem",
-  },
-}
+const navbarIconStyles = "ml-1 mr-3 w-6 h-6"
 
-const Layout = ({ children }) => {
-  return (
-    <div style={styles.container}>
-      <nav style={styles.navbar}>
-        <Link
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "0.9rem",
-            textDecoration: "none",
-          }}
-        >
-          <img src={logo} alt="Logo" style={styles.logo} />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              fontWeight: "bold",
-              fontSize: "1.5rem",
-              textTransform: "uppercase",
-              color: "black",
-            }}
-          >
-            <span>Kenji</span>
-            <span>Kamimura</span>
-          </div>
-        </Link>
-        <ul style={{ marginTop: "0rem", marginLeft: "0" }}>
-          <NavbarItem redirectUrl="/">
-            <TiHomeOutline style={styles.navbarIcons} />
-            Home
-          </NavbarItem>
-          <NavbarItem redirectUrl="/personal-projects">
-            <TiCoffee style={styles.navbarIcons} />
-            Personal Projects
-          </NavbarItem>
-          <NavbarItem redirectUrl="/university-projects">
-            <TiMortarBoard style={styles.navbarIcons} />
-            University Projects
-          </NavbarItem>
-          <NavbarItem redirectUrl="/drone-cinematography">
-            <TiCameraOutline style={styles.navbarIcons} />
-            Drone Cinematography
-          </NavbarItem>
-        </ul>
-      </nav>
-      <div style={styles.main}>
-        <main>{children}</main>
-      </div>
+const Layout = ({ children }) => (
+  <div
+    className="grid grid-rows-1"
+    style={{
+      gridTemplateColumns: "280px auto",
+      minHeight: "100vh",
+    }}
+  >
+    <nav className="border-r border-solid border-gray-400 ">
+      <Link className="m-4 flex justify-center items-center">
+        <img src={logo} alt="Logo" className="w-10 my-2 mr-6 ml-3" />
+        <div className="text-2xl font-bold uppercase flex flex-col justify-between">
+          <span className="-mb-2">Kenji</span>
+          <span className="-mt-2">Kamimura</span>
+        </div>
+      </Link>
+      <ul>
+        <NavbarItem redirectUrl="/">
+          <TiHomeOutline className={navbarIconStyles} />
+          Home
+        </NavbarItem>
+        <NavbarItem redirectUrl="/personal-projects">
+          <TiCoffee className={navbarIconStyles} />
+          Personal Projects
+        </NavbarItem>
+        <NavbarItem redirectUrl="/university-projects">
+          <TiMortarBoard className={navbarIconStyles} />
+          University Projects
+        </NavbarItem>
+        <NavbarItem redirectUrl="/drone-cinematography">
+          <TiCameraOutline className={navbarIconStyles} />
+          Drone Cinematography
+        </NavbarItem>
+      </ul>
+    </nav>
+    <div className="p-4">
+      <main>{children}</main>
     </div>
-  )
-}
+  </div>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
