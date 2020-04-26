14171143d71c0011039871e2529df11064c75e2a
KenjiKamimura <kjp.kamimura@gmail.com>
2020-04-26 14:45:13 +1200

    Add tailwind css

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## Repository for my personal portfolio
 
 - Written using Gatsby.js
-- Hosted on Surge
+- Using Tailwind.css for styles
+- Hosted on AWS S3
 - CICD using github actions
 
 See http://kenji-kamimura-online-portfolio.s3-website-ap-southeast-2.amazonaws.com/ for published website.
--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,15 @@ module.exports = {
         path: `${__dirname}/src/markdown/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
